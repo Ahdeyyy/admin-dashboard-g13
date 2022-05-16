add013ba72534e9d98e50f3795bc662035173617
Jeremiah Elegba <64599754+itz-jherie@users.noreply.github.com>
2022-05-17 00:54:23 +0100

    dashboard page done

---
+++ src/components/Chart.jsx
@@ -0,0 +1,64 @@
+import  React from 'react'
+import {  Line } from "react-chartjs-2";
+import { Chart as ChartJS } from 'chart.js/auto'
+
+
+const Chart = () => {
+    const lineChartData = {
+        labels: ["September","October", "November", "December", "January", "February", "March", "April", "May"],
+        datasets: [
+          {
+            data: [1.9, 1.7, 2.3, 2.2, 1.5, 1.7,1.4, 1.9, 1.2 ],
+            label: "Value in millions",
+            borderColor: "#D65A31",
+            fill: true,
+            lineTension: 0
+          }
+        ]
+        
+      };
+      
+    return ( 
+        <Line
+      type="line"
+      width={10}
+      height={10}
+      options={{
+        title: {
+          display: false,
+          text: "COVID-19 Cases of Last 6 Months",
+          fontSize: 20
+        },
+        legend: {
+          display: false, //Is the legend shown?
+          position: "left" //Position of the legend.
+        },
+        responsive: true, 
+        maintainAspectRatio: false,
+        scales: {
+            x: {
+                ticks: { display: false,
+                color: "#D65A31"},
+                gridLines: {
+                    color: "red",
+                    drawBorder: false
+                }
+            },
+            y: {
+                ticks: { display: true,
+                    color: "#D65A31"
+                 },
+                gridLines: {
+                    display: false,
+                    drawBorder: false
+                }
+        }
+        }
+        
+      }}
+      data={lineChartData}
+    />
+     );
+}
+
+export default Chart;
